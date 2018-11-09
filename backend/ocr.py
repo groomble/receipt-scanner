414da7adf8258868d9a66d2f8741a245d2b19af8
Groomblecom <groomblecom@gmail.com>
2018-11-09 16:09:18 -0500

    Add decision tree classifier for receipt-letters.

--- backend/ocr.py
+++ backend/ocr.py
@@ -2,6 +2,8 @@
 import cv2;
 import numpy as np;
 import pytesseract;
+import os;
+import atexit;
 from math import sin;
 from math import cos;
 from math import sqrt;
@@ -33,23 +35,6 @@ def correctReceipt(pic):
     img_bw = cv2.inRange(img_hsv, (0, 0, 145), (255, max(255-thresh*1.3, 50), 255));
     print("Threshval: %d"%thresh);
     del img_hsv
-
-
-
-    for r in (8, 16, 32, 64):
-        kernel_tuple = [];
-        for i in range(1, r):
-            t = []
-            for j in range(1, r):
-                if (j < r/4 or j > r/4*3) or (i < r/4 or i > r/4*3):
-                    t.append(-1);
-                else:
-                    t.append(0);
-            kernel_tuple.append(t);
-        kernel_tuple = tuple(kernel_tuple);
-        kernel = np.array(kernel_tuple, dtype="int");
-        strays = cv2.morphologyEx(img_bw, cv2.MORPH_HITMISS, kernel); 
-        img_bw = cv2.subtract(img_bw, strays);
 
     contimg, contours, hierarchy = cv2.findContours(img_bw, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     #cv2.drawContours(img, contours, -1, (255,0,0), 3);
@@ -110,22 +95,30 @@ def correctReceipt(pic):
     #find mean of hist:
     H,W = img_bw.shape[:2]
     cv2.namedWindow("result", cv2.WINDOW_NORMAL)
-    def updateBounds(ratio):
-        upd = aligned.copy();
+    def updateBounds(ratio, dim=1, lhist = hist, mimg = aligned):
+        upd = mimg.copy();
         thresh = 0
-        for val in hist:
+        for val in lhist:
             thresh = thresh + val[0]
         thresh = thresh//H
         thresh = thresh * 1000 // (ratio)  
-        print(thresh)
-        uppers = [y for y in range(H-1) if hist[y]<=thresh and hist[y+1]>thresh]
-        lowers = [y for y in range(H-1) if hist[y]>thresh and hist[y+1]<=thresh]
+        print("Threshhold: ", thresh)
+        uppers = [y for y in range(H-1) if lhist[y]<=thresh and lhist[y+1]>thresh]
+        lowers = [y for y in range(H-1) if lhist[y]>thresh and lhist[y+1]<=thresh]
 
-        for y in uppers:
-            cv2.line(upd, (0,y), (W, y), (255,0,0), 1)
+        if(dim == 1):
+            for y in uppers:
+                cv2.line(upd, (0,y), (W, y), (255,0,0), 1)
 
-        for y in lowers:
-            cv2.line(upd, (0,y), (W, y), (0,255,0), 1)
+            for y in lowers:
+                cv2.line(upd, (0,y), (W, y), (0,255,0), 1)
+        else:
+            for x in uppers:
+                cv2.line(upd, (x,0), (x, H), (255,0,0), 1)
+
+            for x in lowers:
+                cv2.line(upd, (x,0), (x, H), (0,255,0), 1)
+
         cv2.imshow("result", upd)
         return (uppers, lowers)
 
@@ -138,16 +131,59 @@ def correctReceipt(pic):
     print(uppers)
     print(lowers)
     lines = []
-    def processLine(i):
-        subimg = img_bw[uppers[i]:lowers[i+1], 0:W]
-        line = pytesseract.image_to_string(subimg)
-        print(line)
+    def processChar(left, right, mimg, mH):
+        subimg = mimg[0:mH, left:right]
+        mW = right-left;
+        # ideal aspect ratio is 2:3 e.g. 32:48
+        if(mW * 2 / 3 - mH > 200):
+            print("Bad character!");
         cv2.namedWindow("result", cv2.WINDOW_NORMAL)
         cv2.imshow("result", subimg)
+        smallimg = cv2.resize(subimg, (32, 48))
+        k = cv2.waitKey()
+        cv2.destroyAllWindows()
+        if not os.path.exists("chars/%c"%k):
+            os.mkdir("chars/%c"%k)
+        cv2.imwrite("chars/%c/%d.png"%(k, processChar.uid), smallimg);
+        processChar.uid += 1
+    processChar.uid = 0
+    with open("chars/count") as f:
+        processChar.uid = int(next(f))
+        processChar.uid += 1
+    def saveUid():
+        processChar.uid += 1 #bad fix for race condition
+        with open("chars/count", "w") as f:
+            f.write("%d"%processChar.uid)
+        print("Exiting on UID %d" %processChar.uid)
+    atexit.register(saveUid)
+
+    def processLine(i):
+        cv2.namedWindow("result", cv2.WINDOW_NORMAL)
+        subimg = img_bw[uppers[i]:lowers[i+1], 0:W]
+        subimg_c = aligned[uppers[i]:lowers[i+1], 0:W]
+        # break into characters via histogramming trick.
+        hist = cv2.reduce(subimg, 0, cv2.REDUCE_AVG).reshape(-1)
+        hist = cv2.GaussianBlur(hist,(5,5),0)
+        #1300 is magic empirical constant
+        char = {'l':[], 'r':[]}
+        def bindUpdateBounds(ratio):
+            (temp1, temp2) = updateBounds(ratio, lhist=hist, mimg = subimg_c, dim=0)
+            print(temp1)
+            char['l'] = temp1;
+            char['r'] = temp2;
+        cv2.createTrackbar("ratio", "result", 1300, 2000, bindUpdateBounds)
+        bindUpdateBounds(1300)
         cv2.waitKey()
         cv2.destroyAllWindows()
+        # armed with individual character images, standardize them if possible
+        # then export to folders.
+        for j, _ in enumerate(char['l']):
+            print("Running character processing")
+            processChar(char['r'][j], char['l'][j], subimg, lowers[i+1]-uppers[i])
+        line= ""
+        print(line)
         return line
-    for i, _ in enumerate(lowers[:-1]):
+    for i, _ in enumerate(lowers[1:-1]):#first is irredeemably weird
         lines.append(processLine(i))
 
 
@@ -166,7 +202,9 @@ def correctReceipt(pic):
 
 print("OpenCV2 version: %s"%cv2.__version__);
 #harvestLine("test/helloworld.png");
-correctReceipt("test-receipts/IMG_20180926_111127.jpg");
+#correctReceipt("test-receipts/IMG_20180926_111127.jpg");
 #correctReceipt("test-receipts/IMG_20180926_112137.jpg");
-correctReceipt("test-receipts/IMG_20181023_200121.jpg");
+#correctReceipt("test-receipts/IMG_20181023_200121.jpg");
+#correctReceipt("test-receipts/image-nate.jpg");
+correctReceipt("test-receipts/15410038188917269064358249794529.jpg");
 
